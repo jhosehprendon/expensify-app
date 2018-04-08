a21261347e7822cc417474b454fb879791d721ce
Jhosehp <jhosehp.rendon@gmail.com>
2018-04-08 18:52:25 -0500

    Set up add expense inputs

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
     <div>
-        Add your expense
+        <h1>Add expense</h1>
+        <ExpenseForm />
     </div>    
 );
 
