37df160ba7119edbf99f8e35c23cbb324fcb9f8c
Jhosehp <jhosehp.rendon@gmail.com>
2018-04-08 19:41:27 -0500

    Add calendar picker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
 
 export default class ExpenseForm extends React.Component {
 
     state= {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
     }
     onDescriptionChange = (e) => {
         const description = e.target.value;
@@ -24,6 +31,14 @@ export default class ExpenseForm extends React.Component {
         }
     }
 
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    }
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }));
+    }
+
     render() {
         return(
             <div>
@@ -40,6 +55,14 @@ export default class ExpenseForm extends React.Component {
                         placeholder="Amount" 
                         value={this.state.amount}  
                         onChange={this.onAmountChange}           
+                    />
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused = {this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={()=> false}
                     />
                     <textarea
                         placeholder="Add your expense note (optional)" 
