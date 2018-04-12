854afdb2e96f62828193b06e41dec38ec51b6334
Jhosehp <jhosehp.rendon@gmail.com>
2018-04-11 23:24:46 -0500

    Add remove and edit expense tests

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
+import { addExpense } from '../../actions/expenses';
 
 test('should render AddExpensePage correctly', () => {
-    const onSubmit = jest.fn();
+    const addExpense = jest.fn();
     const history = { push: jest.fn() };
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+    const wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit', () => {
-    const onSubmit = jest.fn();
+    const addExpense = jest.fn();
     const history = { push: jest.fn() };
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+    const wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+    expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
