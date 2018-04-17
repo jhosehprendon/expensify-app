29fc5f43cfe3c2f8625044ed8791c036a4ef485a
Jhosehp <jhosehp.rendon@gmail.com>
2018-04-17 10:32:42 -0500

    Add styles to expenses list

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,23 +4,26 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-    <div classname="content-container">
-        <div>
-            <div>Expenses</div>
-            <div>Expense</div>
-            <div>Amount</div>
+    <div className="content-container">
+        <div className="list-header">
+            <div className="show-for-mobile">Expenses</div>
+            <div className="show-for-desktop">Expense</div>
+            <div className="show-for-desktop">Amount</div>
         </div>
-        {
-            props.expenses.length === 0 ? (
-                <p>No expenses</p>
-            ) : (
+        <div className="list-body">
+            {
+                props.expenses.length === 0 ? (
+                    <div className="list-item list-item--message">
+                        <span><p>No expenses</p></span>
+                    </div>
+                ) : (
 
-                props.expenses.map((expense)=> {
-                    return <ExpenseListItem key={expense.id} {...expense}/>
-                })
-            )
-        }
-        
+                    props.expenses.map((expense)=> {
+                        return <ExpenseListItem key={expense.id} {...expense}/>
+                    })
+                )
+            }
+        </div>
     </div>    
 );
 
